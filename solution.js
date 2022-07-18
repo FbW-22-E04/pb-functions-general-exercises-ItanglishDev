1ac5b47376cfbad16e496841814bc164b4f31492
chris <chrishethertonweb@gmail.com>
2022-07-18 17:56:43 +0200

    ex 1 done

--- solution.js
+++ solution.js
@@ -1,4 +1,20 @@
-const parameter = "whateva";
-for (let i = 0; i < 3; i++) {
-  console.log(parameter);
+// Ex 1
+
+function takeOneParameter(parameter) {
+  let newParameter = "";
+  for (let i = 0; i <= 3; i++) {
+
+    console.log(parameter);
+  }
 }
+
+takeOneParameter("Cat");
+
+
+// let parameterPlus = "";
+// for (let i = 0; i < 5; i++) {
+//   parameterPlus = "parameter", "parameter";
+//   return parameterPlus;
+// }
+
+// console.log(parameterPlus);
